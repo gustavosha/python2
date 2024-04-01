ad76979213f1ba8bc5a24f5db22309c6e8ab6d55
gustavosha <116170062+gustavosha@users.noreply.github.com>
2024-04-01 12:00:34 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+print ("Programa Expresso")
+print ("*****************")
+print ("1-cadastrar restaurante")
+print ("2-lista restaurante")
+print ("3-ativas restaurrante")
+print ("4-sair")
+
+
+opcaodigitada = int(input("Digite a opção desejada: "))
+
+if(opcaodigitada == 1):
+
+    print ("voce escolheu cadastrar restaurante")
+
+elif (opcaodigitada == 2):
+
+    print ("Você escolheu listar restaurante")
+
+elif (opcaodigitada == 3):
+
+    print ("Você escolheu ativar restaurante")
+
+elif (opcaodigitada == 4):
+
+    print("Você escolheu sair do aplicativo")
+
+else:
+    print ("Essa opçãonão existe escolhaas opções existentes")
